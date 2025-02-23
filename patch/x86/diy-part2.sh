15dc5a1f0cddd3656379db7dc453db3b32bbcc53
yizhanghong <865362414@qq.com>
2025-02-23 10:16:30 +0800

    Update diy-part2.sh

--- patch/x86/diy-part2.sh
+++ patch/x86/diy-part2.sh
@@ -21,7 +21,7 @@ git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status.git package
 # 删除已有配置
 rm -rf configfiles/template/.config_x86-64
 rm -rf router2203.json
-rm -rf package/kernel/mt76
+#rm -rf package/kernel/mt76
 cp -f $GITHUB_WORKSPACE/patch/x86/router2203.json router2203.json
 cp -f $GITHUB_WORKSPACE/patch/x86/config_x86-64.txt configfiles/template/.config_x86-64
 cp -rf $GITHUB_WORKSPACE/patch/x86 configfiles
