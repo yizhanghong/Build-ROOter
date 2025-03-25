472a6984f697b8b75970933513ddce9761035268
yizhanghong <865362414@qq.com>
2025-03-25 13:56:54 +0800

    Update diy-part3.sh

--- patch/x86/diy-part3.sh
+++ patch/x86/diy-part3.sh
@@ -19,23 +19,25 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwa
 git clone --depth=1 https://github.com/gSpotx2f/luci-app-cpu-status.git package/luci-app-cpu-status
 git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
 # 删除已有配置
-#rm -rf configfiles/template/.config_x86-64
+rm -rf configfiles/template/.config_x86-64
 rm -rf router2305.json
 #rm -rf package/kernel/mt76
 cp -f $GITHUB_WORKSPACE/patch/x86/router2305.json router2305.json
-#cp -f $GITHUB_WORKSPACE/patch/x86/config_x86-64.txt configfiles/template/.config_x86-64
+cp -f $GITHUB_WORKSPACE/patch/x86/config_x86-64(2305).txt configfiles/template/.config_x86-64
 cp -rf $GITHUB_WORKSPACE/patch/x86 configfiles
-#cp -rf $GITHUB_WORKSPACE/patch/mt76 package/kernel
+
+# 添加config
+echo "
+CONFIG_PACKAGE_luci-app-statistics=y
+CONFIG_PACKAGE_luci-app-temp-status=y
+CONFIG_PACKAGE_luci-app-cpu-status=y 
+" >> configfiles/template/.config_x86-64
+
 # 禁止进入默认配置
 #sed -i "s/make defconfig/#make defconfig/g" build
 # 禁止build对argon主题修改
 sed -i 's/"$model_argon >/open.png" >/g' build
+# 解决ubim uqmi报错
 sed -i "s/LUCI_DEPENDS/#LUCI_DEPENDS/g" feeds/luci/protocols/luci-proto-mbim/Makefile
 sed -i "s/LUCI_DEPENDS/#LUCI_DEPENDS/g" feeds/luci/protocols/luci-proto-qmi/Makefile
 
-# 升级内核
-#git clone --single-branch -b openwrt-22.03 https://git.openwrt.org/openwrt/openwrt.git newver
-#rm -rf include/kernel-5.10
-#rm -rf target/linux
-#cp -f newver/include/kernel-5.10 include/kernel-5.10
-#cp -rf newver/target/linux target
